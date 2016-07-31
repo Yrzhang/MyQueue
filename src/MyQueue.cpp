a3af84b2e8e82fd5bb9b002968bd372736686b18
Yangran Zhang <yrzhang@hotmail.co.uk>
2016-07-31 20:27:31 +0100

    add move ctor, move assignment, add test case for single thread

--- src/MyQueue.cpp
+++ src/MyQueue.cpp
@@ -7,23 +7,76 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include <cassert>
 #include "MyQueue.hpp"
 
 using namespace std;
 
-int main() {
-	cout << "Start test..." << endl; // prints !!!Hello World!!!
-	MyQueue<int> intQueue(100);
-	intQueue.push(10);
-	intQueue.push(20);
-	intQueue.push(20);
-	intQueue.push(20);
-	intQueue.push(20);
-	intQueue.push(20);
-	int data = 0;
-	while (intQueue.pop_try(ref(data)))
-	{
-		cout << "data = " << data << endl;
-	}
-	return 0;
+namespace
+{
+        struct someData
+        {
+                int i;
+                string str;
+        };
+
+        //create a queue and add a few items
+        MyQueue<someData> createQueue()
+        {
+                MyQueue<someData> dataQue(50);
+                dataQue.push({10, "item 1"});
+                dataQue.push({20, "item 2"});
+                dataQue.push({30, "item 3"});
+                return dataQue;
+        }
+
+        //test constructors and assignment operators in single thread
+        void simpleTestCase()
+        {
+                cout << "simple test case..." << endl;
+                vector<MyQueue<someData> > queVec;
+                for (int i = 0; i < 3; ++i)
+                {
+                        queVec.push_back(createQueue());  //test move constructor
+                }
+
+                //default constrctor
+                MyQueue<someData> queue1;
+
+                //test move assignment operator
+                queue1 = createQueue();
+
+                //test assignment operator
+                MyQueue<someData> queue2;
+                queue2 = queVec[2];
+
+                //test copy constructor
+                MyQueue<someData> queue3(queue2);
+
+                //check values after multiple copies.
+                someData testData;
+                for (int i = 0; i < 3; ++i)
+                {
+                        queue3.pop(testData);
+                }
+
+                assert(testData.i == 30);
+                assert(testData.str == "item 3");
+                assert(queue3.getMaxSize() == 50);
+                assert(queue3.isEmpty() == true);
+
+                //try to pop when the que is empty
+                bool result = queue3.pop_try(testData);
+                assert(result == false);
+
+                cout << "simple tests passed." << endl;
+        }
 }
+
+int main()
+{
+        simpleTestCase();
+        return 0;
+}
+
--- src/MyQueue.hpp
+++ src/MyQueue.hpp
@@ -11,88 +11,137 @@
 #include <mutex>
 #include <queue>
 #include <condition_variable>
+#include <iostream>
 
 template <typename T>
 class MyQueue
 {
 public:
-	MyQueue(int maxSize = 100): m_max_size(maxSize){}
-	//copy constructor;
-	MyQueue(const MyQueue<T>&);
-	virtual ~MyQueue(){}
-	//disallow assignment operator
-	MyQueue& operator=(const MyQueue<T>&) = delete;
+        using simpleLock = std::lock_guard<std::mutex>;
+        using uniqueLock = std::unique_lock<std::mutex>;
 
-	//move constructor
-	//todo
+        MyQueue(int maxSize = 100): m_max_size(maxSize){} //default constructor
+        MyQueue(const MyQueue<T>&);                       //copy constructor;
+        MyQueue(MyQueue<T> &&) noexcept;                  //move constructor
+        MyQueue& operator=(const MyQueue<T>&);            //assignment
+        MyQueue& operator=(MyQueue<T>&& rhs) noexcept;       //move assignment
+        virtual ~MyQueue(){}
 
-	virtual void pop(T& data);
-	virtual bool pop_try(T& data);
-	virtual bool push(T data);
-	virtual bool isEmpty(void) const;
-	virtual int getMaxSize(void) const;
+        virtual void pop(T& data);
+        virtual bool pop_try(T& data);
+        virtual bool push(T data);
+        virtual bool isEmpty(void) const;
+        virtual int getMaxSize(void) const;
 
 private:
-	size_t m_max_size;
-	std::queue<T> m_queue;
-	mutable std::mutex m_mutex;  // define as mutable so that it can be locked in const functions.
-	std::condition_variable m_cond;
+        size_t m_max_size;
+        std::queue<T> m_queue;
+        mutable std::mutex m_mutex;  // define as mutable so that it can be locked in const functions.
+        std::condition_variable m_cond;
 };
 
+//copy constructor
 template<typename T>
-MyQueue<T>::MyQueue(const MyQueue<T>& inQueue)
+MyQueue<T>::MyQueue(const MyQueue<T>& src)
 {
-	std::unique_lock<std::mutex> lock(inQueue.m_mutex);
-	m_queue = inQueue.m_queue;
-	m_max_size = inQueue.m_max_size;
+        std::cout << "copy ctor" << std::endl;
+        simpleLock lock(src.m_mutex);
+        m_queue = src.m_queue;
+        m_max_size = src.m_max_size;
+}
+
+//move constructor
+template<typename T>
+MyQueue<T>::MyQueue(MyQueue<T> && src) noexcept
+{
+        std::cout << "move ctor" << std::endl;
+        simpleLock lock(src.m_mutex);
+        m_queue = std::move(src.m_queue);
+        m_max_size = src.m_max_size;
+}
+
+//assignment
+template<typename T>
+MyQueue<T>& MyQueue<T>::operator=(const MyQueue<T>& rhs)
+{
+        std::cout << "assignment op" << std::endl;
+        //check self assignment
+        if (this == &rhs)
+        {
+                return *this;
+        }
+        std::lock(m_mutex, rhs.m_mutex);
+        simpleLock lock(m_mutex);
+        simpleLock lock_rhs(rhs.m_mutex);
+        m_queue = rhs.m_queue;
+        m_max_size = rhs.m_max_size;
+        return *this;
+}
+
+//move assignment
+template<typename T>
+MyQueue<T>& MyQueue<T>::operator=(MyQueue<T>&& rhs) noexcept
+{
+        std::cout << "move assignment" << std::endl;
+        //check self assignment
+        if (this == &rhs)
+        {
+                return *this;
+        }
+        std::lock(m_mutex, rhs.m_mutex);
+        simpleLock lock(m_mutex);
+        simpleLock lock_rhs(rhs.m_mutex);
+        m_queue = std::move(rhs.m_queue);
+        m_max_size = rhs.m_max_size;
+        return *this;
 }
 
 template<typename T>
 bool MyQueue<T>::push(T data)
 {
-	std::unique_lock<std::mutex> lock(m_mutex);
-	if (m_queue.size() > m_max_size)
-	{
-		return false;
-	}
-	m_queue.push(data);
-	m_cond.notify_one();
-	return true;
+        simpleLock lock(m_mutex);
+        if (m_queue.size() > m_max_size)
+        {
+                return false;
+        }
+        m_queue.push(data);
+        m_cond.notify_one();
+        return true;
 }
 
 template<typename T>
 void MyQueue<T>::pop(T& data)
 {
-	std::unique_lock<std::mutex> lock(m_mutex);
-	m_cond.wait(lock, [this]{return !isEmpty(); });
-	data = m_queue.front();
-	m_queue.pop();
+        uniqueLock lock(m_mutex);
+        m_cond.wait(lock, [this]{return !isEmpty(); });
+        data = m_queue.front();
+        m_queue.pop();
 }
 
 template<typename T>
 bool MyQueue<T>::pop_try(T& data)
 {
-	std::unique_lock<std::mutex> lock(m_mutex);
-	if (isEmpty())
-	{
-		return false;
-	}
-	data = m_queue.front();
-	m_queue.pop();
-	return true;
+        simpleLock lock(m_mutex);
+        if (isEmpty())
+        {
+                return false;
+        }
+        data = m_queue.front();
+        m_queue.pop();
+        return true;
 }
 
 template<typename T>
 bool MyQueue<T>::isEmpty() const
 {
-	std::unique_lock<std::mutex> lock(m_mutex);
-	return m_queue.empty();
+        simpleLock lock(m_mutex);
+        return m_queue.empty();
 }
 
 template<typename T>
 int MyQueue<T>::getMaxSize() const
 {
-	return m_max_size;
+        return m_max_size;
 }
 
 #endif /* MYQUEUE_HPP_ */
